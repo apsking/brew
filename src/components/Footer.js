d07f14c99c0202180dd836a58d7e6830fbfff0af
Alex King <alkin@amazon.com>
2021-04-09 12:25:31 -0500

    Update initial content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/earthandfirebrewing/">@earthandfirebrewing</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          © Copyright {new Date().getFullYear()} All rights reserved. Inspired by{' '}
           <a href="https://thriveweb.com.au/">Thrive</a>.
         </span>
       </div>
