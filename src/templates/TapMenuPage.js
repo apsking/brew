1153e8e1eb772024e8732f0589f2a7780773159a
Alex King <alkin@amazon.com>
2021-04-11 21:19:17 -0500

    Add styling to menu

--- src/templates/TapMenuPage.js
+++ src/templates/TapMenuPage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 
 import TapMenu from '../components/TapMenu'
@@ -7,13 +8,19 @@ import TapMenu from '../components/TapMenu'
 export const TapMenuPageTemplate = ({
   beerlist
 }) => (
-  <main>
-    <section className="section">
-      <div className="container">
-        <TapMenu beerlist={beerlist} />
-      </div>
-    </section>
-  </main>
+  <Fragment>
+    <Helmet>
+      <link href="https://ucarecdn.com" rel="preconnect" crossorigin />
+      <link rel="dns-prefetch" href="https://ucarecdn.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Domine&display=swap" rel="stylesheet"/>
+      {/* Add font link tags here */}
+    </Helmet>
+    <main>
+          <TapMenu beerlist={beerlist} />
+    </main>
+  </Fragment>
+  
 )
 
 const TapMenuPage = ({ data: { page } }) => (
