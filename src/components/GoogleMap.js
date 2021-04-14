f6a51f83480925f751647763a036d06a4ba224d1
Alex King <alkin@amazon.com>
2021-04-14 09:51:38 -0500

    Add google map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,43 +1,16 @@
 import React, { Component } from 'react'
-import GoogleMapReact from 'google-map-react'
-import { MapPin } from 'react-feather'
-
-let mapkey = ''
-if (process.env.GATSBY_NETLIFY_MAP_KEY) {
-  mapkey = process.env.GATSBY_NETLIFY_MAP_KEY
-}
 
 class GoogleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 35.6237711,
-      lng: -120.6867868
-    },
-    zoom: 14
-  }
-
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: mapkey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <Marker lat={35.6237711} lng={-120.6867868} text={'Earth and Fire Brewing'} />
-        </GoogleMapReact>
+      <div class="GoogleMaps--Container">
+        <div class="GoogleMaps--Canvas">
+          <iframe title="Google Maps" width="100%" height="500" id="GoogleMaps--Canvas" src="https://maps.google.com/maps?q=Earth%20and%20Fire%20Brewing%20Company&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        </div>
       </div>
     )
   }
 }
 
 export default GoogleMap
-
-const Marker = () => {
-  return (
-    <div style={{ color: 'red' }}>
-      <MapPin />
-    </div>
-  )
-}
